f7bc314a92b00a8a0198e46e295b27653912b74d
fs203 <fs2031@icloud.com>
2025-06-06 21:43:01 +0900

    clear : francinette

--- get_next_line.c
+++ get_next_line.c
@@ -1,84 +1,5 @@
 #include "get_next_line.h"
 
-void *ft_memcpy(void *dest, const void *src, size_t len)
-{
-	unsigned char *dest_copy = (unsigned char *)dest;
-	const unsigned char *src_copy = (const unsigned char *)src;
-
-	if(dest_copy == src_copy || len == 0)
-		return dest;
-
-	while(len--)
-		*dest_copy++ = *src_copy++;
-	return dest;
-}
-
-size_t ft_strlen(const char *str) 
-{
-  size_t length = 0;
-  while (str[length])
-    length++;
-  return length;
-}
-
-char *ft_strdup(const char *str) {
-  size_t len = ft_strlen(str);
-  char *str_copy = malloc(len + 1);
-  size_t i;
-
-  i = 0;
-  if (!str_copy)
-    return NULL;
-
-  while (len > i) {
-    str_copy[i] = str[i];
-    i++;
-  }
-  str_copy[i] = '\0';
-
-  return str_copy;
-}
-
-
-char *ft_strjoin(char const *s1, char const *s2) 
-{
-  size_t s1_len;
-  size_t s2_len;
-  char *new_str;
-
-  if (!s1 && !s2)
-    return NULL;
-  if (!s1)
-    return ft_strdup(s2);
-  if (!s2)
-    return ft_strdup(s1);
-
-  s1_len = ft_strlen(s1);
-  s2_len = ft_strlen(s2);
-
-  new_str = malloc(s1_len + s2_len + 1);
-  if (!new_str)
-    return NULL;
-
-  ft_memcpy(new_str, s1, s1_len);
-  ft_memcpy(new_str + s1_len, s2, s2_len);
-  new_str[s1_len + s2_len] = '\0';
-  return new_str;
-}
-
-
-char *ft_strrchr(const char *str, int find_ch) 
-{
-  int str_len = ft_strlen(str);
-
-  while (str_len >= 0) {
-    if (str[str_len] == (char)find_ch)
-      return (char *)(str + str_len);
-    str_len--;
-  }
-  return NULL;
-}
-
 char *get_next_line(int fd)
 {
   static char *save_line;
@@ -96,7 +17,7 @@ char *get_next_line(int fd)
   buffer = malloc(BUFFER_SIZE + 1);
 
   if (!buffer)
-      return (NULL);
+    return (NULL);
 
   bytes_read = 1;
   while ((!save_line || !ft_strrchr(save_line, '\n')) && bytes_read != 0)
@@ -105,6 +26,8 @@ char *get_next_line(int fd)
     if (bytes_read == -1)
     {
       free(buffer);
+      free(save_line);
+      save_line = NULL;
       return NULL;
     }
     buffer[bytes_read] = '\0';
@@ -115,9 +38,13 @@ char *get_next_line(int fd)
   free(buffer);
 
   if (!save_line || *save_line == '\0')
+  {
+    free(save_line);
+    save_line = NULL;
     return NULL;
+  }
 
-  while (save_line[len] != '\n')
+  while (save_line[len] && save_line[len] != '\n')
     len++;
   if (save_line[len] == '\n')
     len++;
